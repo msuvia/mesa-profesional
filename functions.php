a82066db340deaa9574f68a69d85a874d6888790
Marcelo Suvia <msuvia@garbarinoviajes.com.ar>
2018-05-15 17:39:06 -0300

    register form fix

--- functions.php
+++ functions.php
@@ -223,8 +223,8 @@ function encryptIt($q) {
     //$cryptKey   = 'qJB0rGtIn5UB1xG';
     //$cryptKey   = 'nDk6dSh3vcX8';
     //$cryptKey   = '97438027782374892';
-    $cryptKey   = '97438027';
-    //$cryptKey   = 'mp2018';
+    //$cryptKey   = '97438027';
+    $cryptKey   = 'mp2018';
     $qEncoded   = base64_encode( mcrypt_encrypt( MCRYPT_RIJNDAEL_256, md5( $cryptKey ), $q, MCRYPT_MODE_CBC, md5( md5( $cryptKey ) ) ) );
     return( $qEncoded );
 }
@@ -238,8 +238,8 @@ function decryptIt($q) {
     //$cryptKey   = 'qJB0rGtIn5UB1xG';
     //$cryptKey   = 'nDk6dSh3vcX8';
     //$cryptKey   = '97438027782374892';
-    $cryptKey   = '97438027';
-    //$cryptKey   = 'mp2018';
+    //$cryptKey   = '97438027';
+    $cryptKey   = 'mp2018';
     $qDecoded   = rtrim( mcrypt_decrypt( MCRYPT_RIJNDAEL_256, md5( $cryptKey ), base64_decode( $q ), MCRYPT_MODE_CBC, md5( md5( $cryptKey ) ) ), "\0");
     return( $qDecoded );
 }
